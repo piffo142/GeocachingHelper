c234228c4fabd86c235be9dee8fe737a56675df6
Steven Griffiths <piffo142@hotmail.com>
2019-05-03 10:30:00 +0100

    Add project files.

---
+++ GeocachingHelper/GeocachingHelper/Models/HomeMenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeocachingHelper.Models
+{
+    public enum MenuItemType
+    {
+        
+        Browse,
+        About,
+        About2
+    }
+    public class HomeMenuItem
+    {
+        public MenuItemType Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
