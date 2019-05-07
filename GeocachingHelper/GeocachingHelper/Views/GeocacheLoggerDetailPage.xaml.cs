823425af7788ecaba945c51d816438d3867c00b3
Steven Griffiths <piffo142@hotmail.com>
2019-05-07 15:58:13 +0100

    Added web scraper

--- GeocachingHelper/GeocachingHelper/Views/GeocacheLoggerDetailPage.xaml.cs
+++ GeocachingHelper/GeocachingHelper/Views/GeocacheLoggerDetailPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -34,12 +35,33 @@ namespace GeocachingHelper.Views
             if (GCCodeEntry != null)
             {
                  DisplayAlert("GC Code is ", GCCodeEntry.Text.ToString(), "ok");
-            } else
+
+
+                string gcInput = GCCodeEntry.Text.ToUpper().ToString();
+                var web = (new HtmlWeb());
+
+                var document = web.Load("http://www.coord.info/" + gcInput);
+
+                //https://coord.info/GC2X2XQ
+
+                var page = document.DocumentNode;
+                try{
+                    string longdesc = document.GetElementbyId("ctl00_ContentBody_LongDescription").InnerText;
+                    string shortdesc = document.GetElementbyId("ctl00_ContentBody_ShortDescription").InnerText;
+                    string CacheName = document.GetElementbyId("ctl00_ContentBody_CacheName").InnerText;
+                    string Hints = document.GetElementbyId("div_hint").InnerText;
+                }
+                catch{
+                    DisplayAlert("Error", "Enter a valid GC Code", "OK");
+                    GCCodeEntry.Focus();
+                }
+            }
+            else
             {
                 DisplayAlert("Error", "Enter a valid GC Code", "OK");
                 GCCodeEntry.Focus();
             }
-            
+           
         }
     }
 }
